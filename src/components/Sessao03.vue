6dd93026d2d1133316f40771bf251617ebcb28ac
Matheus William <matheuswg99@gmail.com>
2020-07-11 01:43:21 -0300

    Criando sessao03.1 Equipe faltando ações sobre fotos da equipe

--- src/components/Sessao03.vue
+++ src/components/Sessao03.vue
@@ -1,88 +1,200 @@
 <template>
-  <v-row class="pb-8" style="background-color: white;">
-    <v-row style="width: 100%;" class="mt-8 mb-8">
-      <v-row style="width: 100%;" class="mb-4">
-        <v-spacer></v-spacer>
-        <h2 style="color:#5E2D5A; font-size: 2.2em;">Quem somos</h2>
-        <v-spacer></v-spacer>
+  <v-row>
+    <!-- QUEM SOMOS -->
+    <v-row class="pb-8" style="background-color: white;">
+      <v-row style="width: 100%;" class="mt-8 mb-8">
+        <v-row style="width: 100%;" class="mb-4">
+          <v-spacer></v-spacer>
+          <h2 style="color:#5E2D5A; font-size: 2.2em;">Quem somos</h2>
+          <v-spacer></v-spacer>
+        </v-row>
+        <v-row style="width: 100%;">
+          <v-spacer></v-spacer>
+          <span style="color:#5E2D5A; text-align:center;">
+            Somos a empresa júnior de turismo na Universidade Federal do Piauí.
+            <br />Atuando no presente momento na cidade de Parnaíba-PI
+          </span>
+          <v-spacer></v-spacer>
+        </v-row>
       </v-row>
-      <v-row style="width: 100%;">
-        <v-spacer></v-spacer>
-        <span style="color:#5E2D5A; text-align:center;">
-          Somos a empresa júnior de turismo na Universidade Federal do Piauí.
-          <br />Atuando no presente momento na cidade de Parnaíba-PI
-        </span>
-        <v-spacer></v-spacer>
+      <v-row>
+        <v-col>
+          <v-row class="mb-6">
+            <v-spacer></v-spacer>
+            <h3 style="color:#5E2D5A; font-size: 2.0em;">Missão</h3>
+            <v-spacer></v-spacer>
+          </v-row>
+          <v-row class="mb-12">
+            <v-spacer></v-spacer>
+            <v-img src="@/assets/sessao03/alvo.png" max-width="180"></v-img>
+            <v-spacer></v-spacer>
+          </v-row>
+          <v-row class="mb-8">
+            <v-spacer></v-spacer>
+            <span style="color: #5E2D5A; text-align:center;">
+              Elaborar atividades de consultoria e prestar
+              <br />serviços no âmbito do turismo regional,
+              <br />objetivando contribuir socialmente
+              <br />para o desenvolvimento local.
+            </span>
+            <v-spacer></v-spacer>
+          </v-row>
+        </v-col>
+        <v-col>
+          <v-row class="mb-6">
+            <v-spacer></v-spacer>
+            <h3 style="color:#5E2D5A;  font-size: 2.0em;">Visão</h3>
+            <v-spacer></v-spacer>
+          </v-row>
+          <v-row class="mb-12">
+            <v-spacer></v-spacer>
+            <v-img src="@/assets/sessao03/visao.png" max-width="180"></v-img>
+            <v-spacer></v-spacer>
+          </v-row>
+          <v-row class="mb-8">
+            <v-spacer></v-spacer>
+            <span style="color: #5E2D5A; text-align:center;">
+              Tornar-se uma empresa reconhecida
+              <br />em seu trabalho com excelência
+              <br />em consultoria, eventos e no
+              <br />desenvolvimento do Turismo em
+              <br />todo o estado do Piauí.
+            </span>
+            <v-spacer></v-spacer>
+          </v-row>
+        </v-col>
+        <v-col>
+          <v-row class="mb-6">
+            <v-spacer></v-spacer>
+            <h3 style="color:#5E2D5A; font-size: 2.0em;">Valores</h3>
+            <v-spacer></v-spacer>
+          </v-row>
+          <v-row class="mb-12">
+            <v-spacer></v-spacer>
+            <v-img src="@/assets/sessao03/valor.png" max-width="180"></v-img>
+            <v-spacer></v-spacer>
+          </v-row>
+          <v-row class="mb-8">
+            <v-spacer></v-spacer>
+            <span style="color: #5E2D5A; text-align:center;">
+              A empresa busca atuar no mercado
+              <br />sempre com ética, responsabilidade,
+              <br />profissionalismo e respeito, levando
+              <br />em consideração o espírito empreendedor.
+            </span>
+            <v-spacer></v-spacer>
+          </v-row>
+        </v-col>
       </v-row>
     </v-row>
-    <v-row>
-      <v-col>
-        <v-row class="mb-6">
+
+    <!-- EQUIPE -->
+
+    <v-row class="pb-8" style="background-color: white;">
+      <v-row style="width: 100%;" class="mt-8 mb-8">
+        <v-row style="width: 100%;" class="mb-2">
           <v-spacer></v-spacer>
-          <h3 style="color:#5E2D5A; font-size: 2.0em;">Missão</h3>
-          <v-spacer></v-spacer>
-        </v-row>
-        <v-row class="mb-12">
-          <v-spacer></v-spacer>
-          <v-img src="@/assets/sessao03/alvo.png" max-width="180"></v-img>
+          <h2 style="color:#5E2D5A; font-size: 2.2em;">Equipe</h2>
           <v-spacer></v-spacer>
         </v-row>
-        <v-row class="mb-8">
+        <v-row style="width: 100%;">
           <v-spacer></v-spacer>
-          <span style="color: #5E2D5A; text-align:center;">
-            Elaborar atividades de consultoria e prestar
-            <br />serviços no âmbito do turismo regional,
-            <br />objetivando contribuir socialmente
-            <br />para o desenvolvimento local.
-          </span>
+          <span
+            style="color:#5E2D5A; text-align:center; font-size: 1.2em;"
+          >Aqueles que fazem tudo funcionar!</span>
           <v-spacer></v-spacer>
         </v-row>
-      </v-col>
-      <v-col>
-        <v-row class="mb-6">
+      </v-row>
+      <v-row class="ml-12">
+        <v-col cols="2">
           <v-spacer></v-spacer>
-          <h3 style="color:#5E2D5A;  font-size: 2.0em;">Visão</h3>
+          <v-img src="@/assets/sessao04/solano-braga.png" max-width="140">
+            <v-row style=" height:100%; background-color: #5E2D5A;  opacity: 0.5;"></v-row>
+          </v-img>
           <v-spacer></v-spacer>
-        </v-row>
-        <v-row class="mb-12">
+        </v-col>
+        <v-col cols="2">
           <v-spacer></v-spacer>
-          <v-img src="@/assets/sessao03/visao.png" max-width="180"></v-img>
+          <v-img src="@/assets/sessao04/arlindo-oliveira.png" max-width="140">
+            <v-row style=" height:100%; background-color: #5E2D5A;  opacity: 0.5;"></v-row>
+          </v-img>
           <v-spacer></v-spacer>
-        </v-row>
-        <v-row class="mb-8">
+        </v-col>
+        <v-col cols="2">
           <v-spacer></v-spacer>
-          <span style="color: #5E2D5A; text-align:center;">
-            Tornar-se uma empresa reconhecida
-            <br />em seu trabalho com excelência
-            <br />em consultoria, eventos e no
-            <br />desenvolvimento do Turismo em
-            <br />todo o estado do Piauí.
-          </span>
+          <v-img src="@/assets/sessao04/conta.png" max-width="140"></v-img>
           <v-spacer></v-spacer>
-        </v-row>
-      </v-col>
-      <v-col>
-        <v-row class="mb-6">
+        </v-col>
+        <v-col cols="2">
           <v-spacer></v-spacer>
-          <h3 style="color:#5E2D5A; font-size: 2.0em;">Valores</h3>
+          <v-img src="@/assets/sessao04/conta.png" max-width="140"></v-img>
           <v-spacer></v-spacer>
-        </v-row>
-        <v-row class="mb-12">
+        </v-col>
+        <v-col cols="2">
           <v-spacer></v-spacer>
-          <v-img src="@/assets/sessao03/valor.png" max-width="180"></v-img>
+          <v-img src="@/assets/sessao04/conta.png" max-width="140"></v-img>
           <v-spacer></v-spacer>
-        </v-row>
-        <v-row class="mb-8">
+        </v-col>
+        <v-col cols="2">
           <v-spacer></v-spacer>
-          <span style="color: #5E2D5A; text-align:center;">
-            A empresa busca atuar no mercado
-            <br />sempre com ética, responsabilidade,
-            <br />profissionalismo e respeito, levando
-            <br />em consideração o espírito empreendedor.
-          </span>
+          <v-img src="@/assets/sessao04/conta.png" max-width="140"></v-img>
           <v-spacer></v-spacer>
-        </v-row>
-      </v-col>
+        </v-col>
+      </v-row>
+      <v-row class="ml-12">
+        <v-col cols="2">
+          <v-spacer></v-spacer>
+          <v-img src="@/assets/sessao04/solano-braga.png" max-width="140">
+            <v-row style=" height:100%; background-color: #5E2D5A;  opacity: 0.5;"></v-row>
+          </v-img>
+          <v-spacer></v-spacer>
+        </v-col>
+        <v-col cols="2">
+          <v-spacer></v-spacer>
+          <v-img src="@/assets/sessao04/conta.png" max-width="140"></v-img>
+          <v-spacer></v-spacer>
+        </v-col>
+        <v-col cols="2">
+          <v-spacer></v-spacer>
+          <v-img src="@/assets/sessao04/conta.png" max-width="140"></v-img>
+          <v-spacer></v-spacer>
+        </v-col>
+        <v-col cols="2">
+          <v-spacer></v-spacer>
+          <v-img src="@/assets/sessao04/conta.png" max-width="140"></v-img>
+          <v-spacer></v-spacer>
+        </v-col>
+        <v-col cols="2">
+          <v-spacer></v-spacer>
+          <v-img
+            src="@/assets/sessao04/arlindo-oliveira.png"
+            max-width="140"
+            v-if="!teste"
+            @click="teste=true"
+          >
+            <v-row style=" height:100%; background-color: #5E2D5A;  opacity: 0.5;"></v-row>
+          </v-img>
+          <v-img
+            src="@/assets/sessao04/arlindo-oliveira.png"
+            max-width="170"
+            v-if="teste"
+            @click="teste=false"
+          >
+            <v-row style="background-color: #5E2D5A; margin-top:90%;" class="pt-2 pb-2">
+              <span style="color:white;" class="ml-6">
+                <strong>Arlindo Oliveira</strong>
+                <br />Presidente
+              </span>
+            </v-row>
+          </v-img>
+          <v-spacer></v-spacer>
+        </v-col>
+        <v-col cols="2">
+          <v-spacer></v-spacer>
+          <v-img src="@/assets/sessao04/conta.png" max-width="140"></v-img>
+          <v-spacer></v-spacer>
+        </v-col>
+      </v-row>
     </v-row>
   </v-row>
 </template>
@@ -91,6 +203,8 @@
 export default {
   name: "Sessao03",
 
-  data: () => ({})
+  data: () => ({
+    teste: false
+  })
 };
 </script>
