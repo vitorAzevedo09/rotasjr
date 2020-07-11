1fcc6a5542fd4b5f812c507adb2b30eec98ff7fc
Matheus William <matheuswg99@gmail.com>
2020-07-10 23:17:24 -0300

    Recriando projeto do zero com Vuetify; Criando Menu; Criando seção01

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-app-bar app color="white" dark height="100">
+      <v-spacer></v-spacer>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Rotasjr Logo"
+          contain
+          src="@/assets/logo_rotas.png"
+          transition="scale-transition"
+          width="180"
+        />
+      </div>
+
+      <v-btn v-for="(item, i) in menu_items" text :key="i">
+        <span class="mr-2" style="color:black;">{{item.title}}</span>
+      </v-btn>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <Sessao01 />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Sessao01 from "./components/Sessao01.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Sessao01
+  },
+
+  data: () => ({
+    menu_items: [
+      { title: "Home", src: null },
+      { title: "Nossos Serviços", src: null },
+      { title: "Quem somos", src: null },
+      { title: "Portfólio", src: null },
+      { title: "Contato", src: null }
+    ]
+  })
+};
+</script>
---
+++ src/components/Sessao01.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row class="text-center">
+    <v-img src="@/assets/sessao01/bg.jpg" contain>
+      <v-row style="height:50%;" align="end" justify="center">
+        <h1 style="color:white; font-size: 2.4em;">Levando o mundo até você!</h1>
+      </v-row>
+      <v-row style="height:50%;" align="start" justify="center">
+        <v-img src="@/assets/sessao01/earth.png" max-width="80"></v-img>
+      </v-row>
+    </v-img>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Sessao01",
+
+  data: () => ({})
+};
+</script>
